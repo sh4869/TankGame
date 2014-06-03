76a9ddbba47d7f9143ff580df61a02048a2d75fc
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-06-03 16:19:05 +0900

    正規表現でわけることまでは出来た

--- serial_read.rb
+++ serial_read.rb
@@ -9,5 +9,30 @@ sp = SerialPort.new(@serial_port,@serial_bps)
 loop do
   read =  sp.read
   puts read
+  /R:/ =~ read
+  val = $'
+  /L:/ =~ val
+  val1 = $`
+  val = $'
+  /H:/ =~ val
+  val2 = $`
+  val = $'
+  /B1:/ =~ val
+  val3 = $`
+  val = $'
+  /B2:/ =~ val
+  val4 = $`
+  val = $'
+  /B3:/ =~ val
+  val5 = $`
+  val6 = $'
+
+  puts val1
+  puts val2 
+  puts val3
+  puts val4
+  puts val5
+  puts val6
+  puts "================="
   sleep(1)
 end
