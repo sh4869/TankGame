239e54b335e787fb701846f684aef36bab7adfc3
sh4869 <nobuk4869@gmail.com>
2014-07-24 15:34:04 +0900

    update tank Arduino program

--- tank/src/sketch.ino
+++ tank/src/sketch.ino
@@ -3,10 +3,10 @@
 author:4869 */
 
 #include<Servo.h>
-
+#include<stdlib.h>
 //定数宣言
 #define MAX 250
-#define NOM 100
+#define NOM 50
 
 //上の回線部分
 const int Turn_A = 2;
@@ -64,94 +64,116 @@ void Turn_Stop(void){
   digitalWrite(Turn_B,LOW);
 }
 
-//右側モーターに関する関数
-//右のモーターを前に回転させる
-void Right_go(String val){
-  if(val != "x"){
-	int vali = atoi(val);
-	if(vali == 5){
-	  digitalWrite(Right_A,LOW);
-	  digitalWrite(Right_B,LOW);
-	}else(vali > 5){
-	  digitalWrite(Right_A,HIGH);
-	  digitalWrite(Right_B,LOW);
-	  analogWrite(Right_PWM,NOM + ((vali - 5) * 10)); 
-	}else{  
-	  digitalWrite(Right_A,LOW);
-	  digitalWrite(Right_B,HIGH);
-	  analogWrite(Right_PWM,NOM + (-(vali - 6) * 10));
-	}
+void battery(char val){
+  if(val != 'x'){
+	int vali = elect(val);
+	vali = vali * 10;
+	myservo.write(vali);
   }
 }
-  //左側のモーターに関する関数
-void Left_go(String val){
-  if(val != "x"){
-	int vali = atoi(val);
-	if(vali == 5){
+
+//右側モーターに関する関数
+void Right_go(char val){
+  int vali = elect(val);
+  Serial.println(vali);
+  if(vali == 5){
+	digitalWrite(Right_A,LOW);
+	digitalWrite(Right_B,LOW);
+  }else if(vali > 5){
+	digitalWrite(Right_A,HIGH);
+	digitalWrite(Right_B,LOW);
+	analogWrite(Right_PWM,NOM + ((vali - 5) * 20)); 
+  }else{  
+	digitalWrite(Right_A,LOW);
+	digitalWrite(Right_B,HIGH);
+	vali = vali - 6;
+	analogWrite(Right_PWM,NOM+ (-vali * 20));
+  }
+}
+//左側のモーターに関する関数
+void Left_go(char val){
+  int vali = elect(val);
+  Serial.println(vali);
+  if(vali == 5){
 	digitalWrite(Left_A,LOW);
 	digitalWrite(Left_B,LOW);
-	}else(vali > 5){
+  }else if(vali > 5){
 	digitalWrite(Left_A,HIGH);
 	digitalWrite(Left_B,LOW);
-	analogWrite(Left_PWM,NOM + ((vali - 5) * 10));
-    }else{
+	analogWrite(Left_PWM,NOM + ((vali - 5) * 20));
+  }else{
 	digitalWrite(Left_A,LOW);
 	digitalWrite(Left_B,HIGH);
-	analogWrite(Left_PWM,NOM + (-(vali - 6) * 10));
-	}
+	vali = vali - 6;
+	analogWrite(Left_PWM,NOM + (-vali  * 10));
   }
 }
-  //砲台の上下に関する関数
+//砲台の上下に関する関数
 
+int elect(char x){
+  switch(x){
+	case '1':return 1;break;
+	case '2':return 2;break;
+	case '3':return 3;break;
+	case '4':return 4;break;
+	case '5':return 5;break;
+	case '6':return 6;break;
+	case '7':return 7;break;
+	case '8':return 8;break;
+	case '9':return 9;break;
+  }
+}
 
+void loop(){
+  while(Serial.available()){
+	ch = Serial.read();
+	switch(ch){
+	  //Car Right Moter
+	  case 'R':{
+				 delay(10);
+				 ch = Serial.read();
+				 Serial.println(ch);
+				 Right_go(ch);
+				 break;
+			   }  
+			   //Car Left Moter
+	  case 'L':{ 
+				 delay(10);
+				 ch = Serial.read();
+				 Left_go(ch);
+				 break;
+			   } 
+			   //Car battery's servo moter
+	  case 'H':{ 
+				 ch = Serial.read();
 
-  void loop(){
-	while(Serial.available()){
-	  ch = Serial.read();
-	  switch(ch){
-		//Car Right Moter
-		case 'R':{
-				   ch = Serial.read();
-
-				   break;
-				 }  
-				 //Car Left Moter
-		case 'L':{ 
-				   ch = Serial.read();
-				   break;
-				 } 
-				 //Car battery's servo moter
-		case 'H':{ 
-				   ch = Serial.read();
-
-				   break;
+				 break;
+			   }
+			   //B1:Battery right moter B2:Battery left Moter B3:Firing
+	  case 'B':{
+				 ch = Serial.read();
+				 if(ch == '1'){
+				   Turn_Right();
+				   r = 1;
+				 }else{
+				   r = 0;
 				 }
-				 //B1:Battery right moter B2:Battery left Moter B3:Firing
-		case 'B':{
-				   ch = Serial.read();
-				   if(ch == "1"){
-					 Turn_Right();
-					 r = 1;
-				   }else{
-					 r = 0;
-				   }
-				   ch = Serial.read();
-				   if(ch == "1"){
-					 Turn_Lefti();
-					 l = 1;
-				   }else{
-					 l = 0;
-				   }
-				   if(r == 0 && l == 0){
-					 Turn_Stop();
-				   }
-				   ch = Serial.read();
-				   if(ch == "1"){
-
-				   }
-				   break;
+				 ch = Serial.read();
+				 if(ch == '1'){
+				   Turn_Left();
+				   l = 1;
+				 }else{
+				   l = 0;
 				 }
-	  }
+				 if(r == 0 && l == 0){
+				   Turn_Stop();
+				 }
+				 ch = Serial.read();
+				 if(ch == '1'){
+
+				 }
+				 break;
+			   }
 	}
   }
 }
