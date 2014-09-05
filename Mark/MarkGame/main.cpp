17d7156d235a742487546638871e0d8ea9318119
sh4869 <nobuk4869@gmail.com>
2014-09-05 23:39:07 +0900

    add basicdraws sample

--- Mark/MarkGame/main.cpp
+++ Mark/MarkGame/main.cpp
@@ -1,10 +1,14 @@
-#include<QtWidgets>
-#include "gamewindow.h"
+
+#include "window.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-  QApplication app(argc,argv);
-  GameWindow gamewindow;
-  gamewindow.show(); 
-  return app.exec();
+    Q_INIT_RESOURCE(basicdrawing);
+
+    QApplication app(argc, argv);
+    Window window;
+    window.show();
+    return app.exec();
 }
