f01e59f5a3ec8ea7a0d86fff34eb221ec64caa19
sh4869 <nobuk4869@gmail.com>
2014-09-07 12:32:03 +0900

    make first project

--- Mark/MarkGame/main.cpp
+++ Mark/MarkGame/main.cpp
@@ -1,14 +1,10 @@
-
 #include "window.h"
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    Q_INIT_RESOURCE(basicdrawing);
-
-    QApplication app(argc, argv);
-    Window window;
-    window.show();
-    return app.exec();
+int main(int argc,char *argv[]){
+  QApplication app(argc,argv);
+  Window window;
+  window.show();
+  return app.exec();
 }
